a352a9be9c90bf869a6099f6c2c11df008ecaf1d
Rohitmalviya657 <140175677+Rohitmalviya657@users.noreply.github.com>
2024-09-03 14:53:37 +0530

    comit5

--- model/Roomsavailabler.js
+++ model/Roomsavailabler.js
@@ -1,6 +1,8 @@
 import { DataTypes } from "sequelize";
+// import sequelize from "../db/dbconfig.js";
 import sequelize from "../db/dbconfig.js";
-
+import bcrypt from 'bcryptjs';
+import { Result } from "express-validator";
 const RoomsAvailable = sequelize.define("Rooms", {
     Roomid: {
         type: DataTypes.INTEGER,
@@ -9,32 +11,37 @@ const RoomsAvailable = sequelize.define("Rooms", {
     },
     description: {
         type: DataTypes.STRING,
+
+
     },
     price: {
         type: DataTypes.INTEGER,
+
     },
     location: {
         type: DataTypes.STRING,
+
+
     },
     size: {
         type: DataTypes.STRING,
+
     },
+    // imgUrl: {
+    //     type: DataTypes.STRING,
+    //     allowNull: false, 
+    //     validate: {
+    //         isUrl: true 
+    //     }
     imgUrl: {
-        type: DataTypes.STRING,
+        type: DataTypes.STRING, // <- type for image ( database :postgresql )
         allowNull: true
     },
-    booked: {  // New field to track booking status
-        type: DataTypes.BOOLEAN,
-        defaultValue: false
-    }
+
 });
-
-sequelize.sync()
-    .then(result => {
-        console.log("roomsavailable table Created---");
-    })
-    .catch(err => {
-        console.log("Something wrong in roomsavailable---", err);
-    });
-
+sequelize.sync().then(result => {
+    console.log("roomsavailable table Created---");
+}).catch(err => {
+    console.log("Something wrong in roomsavailable---");
+})
 export default RoomsAvailable;
